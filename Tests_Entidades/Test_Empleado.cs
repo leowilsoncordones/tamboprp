1a8bc6ada46393fd72260fbb31487b0da14c81c7
leowilson <leowilsoncordones@hotmail.com>
2014-10-29 21:06:25 -0200

    Corrijo properties de entidades, agrego constructores de entidades, creo libreria de clases y coloco las entidades ahi.

---
+++ Tests_Entidades/Test_Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace Tests_Entidades
+{
+    [TestFixture]
+    public class TestEmpleado
+    {
+
+        [Test]
+        public void empleado_should_have_id()
+        {
+            var emp = new Empleado{Id_empleado = 1, Activo = true, Apellido = "Perez", Nombre = "Juan" };
+            var result = emp.Id_empleado.ToString();
+            Assert.IsNotEmpty(result);
+        }
+
+    }
+}
