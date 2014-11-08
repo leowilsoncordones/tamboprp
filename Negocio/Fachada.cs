bfe5b8ef4958a3394799034813aeeb2456303a44
leowilson <leowilsoncordones@hotmail.com>
2014-11-08 13:17:33 -0200

    subo Negocio con fachada

---
+++ Negocio/Fachada.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Negocio
+{
+    public class Fachada
+    {
+        private static Fachada _instance;
+
+        private Fachada()
+        {
+        }
+
+        public static Fachada Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Fachada();
+                }
+                return _instance;
+            }
+        }
+
+    }
+}
