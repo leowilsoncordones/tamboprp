a588c2b6f0ed213256c9a74c9f3aadbea339d4e9
leowilson <leowilsoncordones@hotmail.com>
2014-11-11 22:27:04 -0200

    Agrego indo a datos generales y test serialize to json

--- Datos/AnimalMapper.cs
+++ Datos/AnimalMapper.cs
@@ -164,5 +164,19 @@ namespace Datos
             return anim;
         }
 
+        public int GetCantOrdene()
+        {
+            return GetScalarInt("Animal_SelectCountOrdene");
+        }
+
+        public int GetCantEntoradas()
+        {
+            return GetScalarInt("Animal_SelectCountEntorada");
+        }
+
+        public int GetCantSecas()
+        {
+            return GetScalarInt("Animal_SelectCountSeca");
+        }
     }
 }
